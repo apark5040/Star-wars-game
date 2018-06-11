173e833aac2987eaedfa359bc4aa458ae1ec493e
Andrew Park <apark11278@gmail.com>
2018-06-10 20:50:33 -0400

    Added attack button function and win/lose scenario

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,69 @@
 var luke = {
-    hp: 200,
+    hp: 500,
     attack: 10,
-    defense: 5
+    counterAttack: 5,
+    name: "assets/images/Luke.jpg"
 };
 
 var darthVader = {
-    hp: 200,
+    hp: 100,
     attack: 10,
-    defense: 5
+    counterAttack: 5,
+    name: "assets/images/Vader.png"
 };
+
+var stormTrooper = {
+    hp: 100,
+    attack: 10,
+    counterAttack: 5,
+    name: "assets/images/Stormtrooper.jpg"
+}
+
+var maceWindu = {
+    hp: 100,
+    attack: 10,
+    counterAttack: 5,
+    name: "assets/images/Macewindu.jpg"
+}
+
+var hanSolo = {
+    hp: 100,
+    attack: 10,
+    counterAttack: 5,
+    name: "assets/images/Hansolo.jpg"
+}
+
+var grievous = {
+    hp: 100,
+    attack: 10,
+    counterAttack: 5,
+    name: "assets/images/Grievous.jpg"
+}
 
 var isCharPicked = false;
 var isOpponentPicked = false;
 
+var downChar;
+var userChar;
+var enemyChar;
 
-var lukePic = $("<img>");
-lukePic.attr("src", "assets/images/Luke.jpg");
-lukePic.addClass("characters lukeSky col-md-2");
-$("#game").append(lukePic);
+var wins = 0;
 
-var vaderPic = $("<img>");
-vaderPic.attr("src", "assets/images/Vader.png");
-vaderPic.addClass("characters darthVad col-md-2");
-$("#game").append(vaderPic);
 
-// var obiwanPic = $("<img>");
-// obiwanPic.attr("src", "assets/images/Obiwan.jpg");
-// obiwanPic.addClass("characters obiwanKen col-md-2");
-// $("#game").append(obiwanPic);
+function picAdd(name) {
+    var pic = $("<img>");
+    pic.attr("src", name);
+    pic.addClass("characters col-md-2");
+    $("#game").append(pic);
+}
 
-var stormPic = $("<img>");
-stormPic.attr("src", "assets/images/Stormtrooper.jpg");
-stormPic.addClass("characters stormTroop col-md-2");
-$("#game").append(stormPic);
+picAdd(luke.name);
+picAdd(darthVader.name);
+// picAdd(stormTrooper.name);
+picAdd(maceWindu.name);
+// picAdd(hanSolo.name);
+picAdd(grievous.name);
 
-var macePic = $("<img>");
-macePic.attr("src", "assets/images/Macewindu.jpg");
-macePic.addClass("characters maceWin col-md-2");
-$("#game").append(macePic);
-
-// var sidPic = $("<img>");
-// sidPic.attr("src", "assets/images/Sidious.jpg");
-// sidPic.addClass("characters darthSid col-md-2");
-// $("#game").append(sidPic);
-
-var hanPic = $("<img>");
-hanPic.attr("src", "assets/images/Hansolo.jpg");
-hanPic.addClass("characters hanSolo col-md-2");
-$("#game").append(hanPic);
-
-var grieviousPic = $("<img>");
-grieviousPic.attr("src", "assets/images/Grievous.jpg");
-grieviousPic.addClass("characters GeneralGrie col-md-2");
-$("#game").append(grieviousPic);
 
 
 $(".characters").click(function () {
@@ -66,29 +77,106 @@ $(".characters").click(function () {
         isCharPicked = true;
         $(this).addClass("hide");
 
+        if ($(this).attr("src") === luke.name) {
+            userChar = luke;
+        }
+        if ($(this).attr("src") === darthVader.name) {
+            userChar = darthVader;
+        }
+        if ($(this).attr("src") === stormTrooper.name) {
+            userChar = stormTrooper;
+        }
+        if ($(this).attr("src") === maceWindu.name) {
+            userChar = maceWindu;
+        }
+        if ($(this).attr("src") === hanSolo.name) {
+            userChar = hanSolo;
+        }
+        if ($(this).attr("src") === grievous.name) {
+            userChar = grievous;
+        }
+       $("#pickChar").remove();
+
     }
     //This happens when you pick your character. This part is for picking your opponent.
-    else{
+    else {
         isOpponentPicked = true;
-        var downChar = $("<img>");
+        downChar = $("<img>");
         var imagePath = $(this).attr("src");
         downChar.attr("src", imagePath);
         downChar.addClass("col-md-2 computer");
         $("#lower").append(downChar);
         $(this).addClass("hide");
 
+        if ($(this).attr("src") === luke.name) {
+            enemyChar = luke;
+
+        }
+        if ($(this).attr("src") === darthVader.name) {
+            enemyChar = darthVader;
+        }
+        if ($(this).attr("src") === stormTrooper.name) {
+            enemyChar = stormTrooper;
+        }
+        if ($(this).attr("src") === maceWindu.name) {
+            enemyChar = maceWindu;
+        }
+        if ($(this).attr("src") === hanSolo.name) {
+            enemyChar = hanSolo;
+        }
+        if ($(this).attr("src") === grievous.name) {
+            enemyChar = grievous;
+        }
+
     }
-    
-    
-    if(isCharPicked && isOpponentPicked){
+
+
+    if (isCharPicked && isOpponentPicked) {
         $("#game").addClass("hide");
     }
 
 });
 
-$("#attack").click(function(){
-    //Test to see if rest of the charcters can come back after click
-    $("#game").removeClass("hide");
+$("#attack").click(function () {
+
+    if (isCharPicked && isOpponentPicked) {
+
+        userChar.hp = userChar.hp - enemyChar.counterAttack;
+        enemyChar.hp = enemyChar.hp - userChar.attack;
+
+        var messageLog = $("<div>");
+        $("#message").html(messageLog);
+        messageLog.attr("id", "log");
+        messageLog.attr("class", "battleLog");
+        messageLog.html("<p>You dealt " + userChar.attack + " and brought down your opponents HP to " + enemyChar.hp + "</p>" +
+            "<p>Your opponent dealt " + enemyChar.counterAttack + " and brought your HP down to " + userChar.hp + "</p>");
+
+        userChar.attack = userChar.attack + 5;
+
+        if (enemyChar.hp <= 0) {
+            $("#game").removeClass("hide");
+            messageLog.removeClass("battleLog");
+            messageLog.empty();
+            isOpponentPicked = false;
+            downChar.remove();
+            enemyChar;
+            wins++;
+            
+            
+        }
+        if(userChar.hp <= 0){
+            alert("You lose");
+        }
+        
+    }
+
+    if(wins === 3){
+        messageLog.attr("class", "battleLog");
+        messageLog.html("<p>You win!</p>");
+    }
+
 });
+
+
 
 
